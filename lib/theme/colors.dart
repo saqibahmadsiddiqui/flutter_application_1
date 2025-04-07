a242764b158f1e54a6a979c1a15451a7742c0068
Muhammad Muzamil Khan <77438943+MuzmmilKhan1@users.noreply.github.com>
2025-04-07 06:18:13 -0700

    travenor

---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0D6EFD); // Fixed: added FF for full opacity
+  static const Color secondary = Color(0xFFFF7029);
+  static const Color background = Color(0xFFFFFFFF); // Fixed: added FF at the start
+  static const Color text = Color(0xFF1B1E28);
+  static const Color grey = Color(0xFF7D848D);
+}
